be6b92282f047b90fa57c031416f21dc87e1493e
Marx Medeiros <analista.marx@gmail.com>
2016-10-26 11:07:55 -0200

    Troca de Pasta

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* config.php */
-function dbcon()
-{
-    @mysql_connect("localhost:8889", "root", "root") or die(mysql_error());
-    @mysql_select_db("producao") or die(mysql_error());
-}
-?>
